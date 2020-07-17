a07b8d6c8a275e48ce5e087050afba5a3e61ba6f
Tobias Wiesner <tobias@tawiesn.de>
2020-07-17 23:02:37 +0200

    hog: add pyramid and window sliding routines

--- src/image_processing/bin/demo.py
+++ src/image_processing/bin/demo.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import sys
+import time
+import imutils
 
 import cv2
 import numpy as np
@@ -36,8 +38,8 @@ def process_frame(object_detector, image):
             top  = box[1]
             right = box[2]
             bottom = box[3]
-            right = int(box[2]+20)
-            bottom = int(box[3]+25)
+            right = int(box[2])
+            bottom = int(box[3])
             cv2.rectangle(image, (left,top), (right,bottom), (255,178,50),3)
             label = '%s:%.2f' % (object_detector.label(class_id), confidence)
             label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
@@ -50,10 +52,37 @@ def process_frame(object_detector, image):
         
     return image
 
+def sliding_window(image, step, window_size):
+    # slide a window across the image
+    for y in range(0, image.shape[0] - window_size[1], step):
+        for x in range(0, image.shape[1] - window_size[0], step):
+            # yield the current window
+            yield (x, y, image[y:y + window_size[1], x:x + window_size[0]])
+
+def image_pyramid(image, scale=1.5, min_size=(224, 224)):
+    # yield the original image
+    yield image
+    # keep looping over the image pyramid
+    while True:
+        # compute the dimensions of the next image in the pyramid
+        w = int(image.shape[1] / scale)
+        image = imutils.resize(image, width=w)
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < min_size[1] or image.shape[1] < min_size[0]:
+                break
+        # yield the next image in the pyramid
+        yield image
+
+PYRAMID_SCALE = 2.0
+WIN_STEP = 416 #312
+ROI_SIZE = (1232,1232)
+INPUT_SIZE = (416,416)
+
 def main(**kwargs):
 
 
-    confidence = 0.3
+    confidence = 0.1
     suppression_threshold = 0.4
     object_detector = ObjectDetectorYolo(kwargs["yolo"], confidence, suppression_threshold)
 
@@ -63,34 +92,77 @@ def main(**kwargs):
     datatype = image.dtype
     image_8bit = list(map(np.uint8, image)) if datatype != np.uint8 else image
 
+   
+   # initialize the image pyramid
+    pyramid = image_pyramid(image_8bit, scale=PYRAMID_SCALE, minSize=ROI_SIZE)
+    # initialize two lists, one to hold the ROIs generated from the image
+    # pyramid and sliding window, and another list used to store the
+    # (x, y)-coordinates of where the ROI was in the original image
+    rois = []
+    locs = []
+   
+    cnt = 0
     
+    # loop over the image pyramid
+    for image in pyramid:
+        # determine the scale factor between the *original* image
+        # dimensions and the *current* layer of the pyramid
+        scale = W / float(image.shape[1])
+        # for each layer of the image pyramid, loop over the sliding
+        # window locations
+        for (x, y, roiOrig) in sliding_window(image, WIN_STEP, ROI_SIZE):
+            # scale the (x, y)-coordinates of the ROI with respect to the
+            # *original* image dimensions
+            x = int(x * scale)
+            y = int(y * scale)
+            w = int(ROI_SIZE[0] * scale)
+            h = int(ROI_SIZE[1] * scale)
+            # take the ROI and preprocess it so we can later classify
+            # the region using Keras/TensorFlow
+            roi = cv2.resize(roiOrig, INPUT_SIZE)
+            
+            print("ROI: {} = {}".format(cnt,roi))
+            
+            #roi = img_to_array(roi)
+            
+            plt.imshow(roi[..., ::-1])
+            plt.show()
+    
+            #roi = preprocess_input(roi)
+            # update our list of ROIs and associated coordinates
+            rois.append(roi)
+            locs.append((x, y, x + w, y + h))   
+            
+            cnt += 1
+   
+    print("#ROIs = {}".format(len(rois)))
+   
+    ## detect people mirrored in lake
+    #image_8bit = cv2.flip(image_8bit, 0)
+    #img_part = image_8bit[1730:1730+500, 5340:5340+500].copy()
+    #img_part_detected = process_frame(object_detector, img_part)
+    #plt.imshow(img_part_detected[..., ::-1])
+    #plt.show()
 
-    # detect people mirrored in lake
-    image_8bit = cv2.flip(image_8bit, 0)
-    img_part = image_8bit[1730:1730+500, 5340:5340+500].copy()
-    img_part_detected = process_frame(object_detector, img_part)
-    #img_part_detected = cv2.flip(img_part_detected, 0)
-    plt.imshow(img_part_detected[..., ::-1])
-    plt.show()
-
-    # detect people at lake (including the guy behind the hill)
-    image_8bit = cv2.flip(image_8bit, 0)
-    img_part = image_8bit[1840:1840+500, 5340:5340+500].copy()
+    ## detect people at lake (including the guy behind the hill)
+    #image_8bit = cv2.flip(image_8bit, 0)
+    #img_part = image_8bit[1840:1840+500, 5340:5340+500].copy()
+    img_part = image_8bit[1620:1840+1232, 5340:5340+1232].copy()
     img_part_detected = process_frame(object_detector, img_part)
     plt.imshow(img_part_detected[..., ::-1])
     plt.show()
     
-    # detect guy on the right border of image
-    img_part = image_8bit[1600:1600+500, 5600:5600+500].copy()
-    img_part_detected = process_frame(object_detector, img_part)
-    plt.imshow(img_part_detected[..., ::-1])
-    plt.show()
+    ## detect guy on the right border of image
+    #img_part = image_8bit[1600:1600+500, 5600:5600+500].copy()
+    #img_part_detected = process_frame(object_detector, img_part)
+    #plt.imshow(img_part_detected[..., ::-1])
+    #plt.show()
     
-    # apply detector to full image
-    img_part = image_8bit.copy()
-    img_part_detected = process_frame(object_detector, img_part)
-    plt.imshow(img_part_detected[..., ::-1])
-    plt.show()
+    ## apply detector to full image
+    #img_part = image_8bit.copy()
+    #img_part_detected = process_frame(object_detector, img_part)
+    #plt.imshow(img_part_detected[..., ::-1])
+    #plt.show()
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
