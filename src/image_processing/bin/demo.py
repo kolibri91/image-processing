752e82b1737db42ee952e1e7bb9c95853abc5b43
Tobias Wiesner <tobias@tawiesn.de>
2020-07-31 22:35:11 +0200

    object-detection: clean up demo program

--- src/image_processing/bin/demo.py
+++ src/image_processing/bin/demo.py
@@ -11,7 +11,7 @@ from image_processing.io import read_image, write_image
 from matplotlib import pyplot as plt
 
 from object_detection.object_detector_yolo import ObjectDetectorYolo
-
+from object_detection.object_detector_pyramid import ObjectDetectorPyramid
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -29,60 +29,6 @@ def parse_command_line_args(args):
     return vars(parsed_args)
 
 
-def sliding_window(image, step, window_size):
-    xx = list(range(0, image.shape[1] - window_size[0], step))
-    xx.append(image.shape[1]-window_size[0])
-    yy = list(range(0, image.shape[0] - window_size[1], step))
-    yy.append(image.shape[0]-window_size[1])
-
-    # slide a window across the image
-    for y in yy:
-        for x in xx:
-            yield (x, y, image[y:y + window_size[1], x:x + window_size[0]])
-
-
-def image_pyramid(image, scale=1.5, min_size=(224, 224)):
-    # yield the original image
-    yield image
-    # keep looping over the image pyramid
-    while True:
-        # compute the dimensions of the next image in the pyramid
-        w = int(image.shape[1] / scale)
-        image = imutils.resize(image, width=w)
-        # if the resized image does not meet the supplied minimum
-        # size, then stop constructing the pyramid
-        if image.shape[0] < min_size[1] or image.shape[1] < min_size[0]:
-            break
-        # yield the next image in the pyramid
-        yield image
-
-
-def find_objects_and_return_object_informations(object_detector, frame, scaling_factor):
-    objects_detected = object_detector.process_frame(frame)
-    (object_class_ids, object_confidences, object_boxes) = object_detector.postprocess_frame(frame, objects_detected)
-
-    bounding_boxes = []
-    classes = []
-    confidences = []
-    for (class_id, confidence, box) in zip(object_class_ids, object_confidences, object_boxes):
-        if object_detector.label(class_id) in ('person','backpack'):
-            left = int(float(box[0]) * scaling_factor)
-            top  = int(float(box[1]) * scaling_factor)
-            right = int(float(box[2]) * scaling_factor)
-            bottom = int(float(box[3]) * scaling_factor)
-            bounding_boxes.append((left,top,right,bottom))
-            classes.append(class_id)
-            confidences.append(confidence)
-            
-    return (bounding_boxes, classes, confidences)
-
-
-PYRAMID_SCALE = 2.0
-WIN_STEP = 416 #312
-ROI_SIZE = (1232,1232)
-INPUT_SIZE = (416,416)
-
-
 def main(**kwargs):
 
 
@@ -92,83 +38,18 @@ def main(**kwargs):
 
     print("[INFO] Read image from disk...")
     image = cv2.imread(kwargs["input"])
-    (H, W) = image.shape[:2]
+
     datatype = image.dtype
     image_8bit = list(map(np.uint8, image)) if datatype != np.uint8 else image
 
-   
-   # initialize the image pyramid
-    pyramid = image_pyramid(image_8bit, scale=PYRAMID_SCALE, min_size=ROI_SIZE)
-    # initialize two lists, one to hold the ROIs generated from the image
-    # pyramid and sliding window, and another list used to store the
-    # (x, y)-coordinates of where the ROI was in the original image
-    rois = []
-    locs = []
-    objects = []
-   
-    cnt = 0
-    
-    # loop over the image pyramid
-    for image in pyramid:
-        # determine the scale factor between the *original* image
-        # dimensions and the *current* layer of the pyramid
-        scale = W / float(image.shape[1])
-        # for each layer of the image pyramid, loop over the sliding
-        # window locations
-        for (x, y, roi_orig) in sliding_window(image, WIN_STEP, ROI_SIZE):
-            # scale the (x, y)-coordinates of the ROI with respect to the
-            # *original* image dimensions
-            x = int(x * scale)
-            y = int(y * scale)
-            w = int(ROI_SIZE[0] * scale)
-            h = int(ROI_SIZE[1] * scale)
-            # take the ROI and preprocess it so we can later classify
-            # the region using Keras/TensorFlow
-            roi = cv2.resize(roi_orig, INPUT_SIZE)
-            
-    
-            #roi = preprocess_input(roi)
-            # update our list of ROIs and associated coordinates
-            rois.append(roi)
-            locs.append((x, y, x + w, y + h))   
-            
-            
-            check_for_mirrors = False
-            
-            (bounding_boxes, classes, confidences) =  find_objects_and_return_object_informations(object_detector, roi_orig, scale)
-         
-            for (bb, class_id, confidence) in zip(bounding_boxes, classes, confidences):
-                objects.append(((x+bb[0],y+bb[1]),(x+bb[2],y+bb[3]),class_id, confidence))
-                check_for_mirrors = True
-                    
-            if check_for_mirrors == True:
-                # flip image and reprocess it
-                roi_flipped = roi_orig.copy()
-                roi_flipped = cv2.flip(roi_flipped, 0)               
-
-                (bounding_boxes, classes, confidences) =  find_objects_and_return_object_informations(object_detector, roi_flipped, scale)
-                for (bb, flipped_class_id, flipped_confidence) in zip(bounding_boxes, classes, confidences):
-                
-                    objects.append(((x+bb[0],y+h-bb[3]),(x+bb[2],y+h-bb[1]),flipped_class_id, flipped_confidence))
-                
-                    
-            # show the visualization and current ROI
-            #clone_resized = imutils.resize(clone, width=1024)
-            #cv2.imshow("Visualization", clone_resized)
-            ##cv2.imshow("ROI", roiOrig)
-            #cv2.waitKey(1)
-
-            #print("ROI: {} roi: {}".format(roiOrig.shape,roi.shape))
-            cnt += 1
-   
-    print("#ROIs = {}".format(len(rois)))
+    object_detector_pyramid = ObjectDetectorPyramid(object_detector)
+    objects = object_detector_pyramid.process_frame(image_8bit)
    
     result = image_8bit.copy()
     for o in objects:
-        cv2.rectangle(result, o[0], o[1], (255,178,50),8)
-        print("Object: {} Confidence: {} Box: ({})-({})".format(o[2],o[3],o[0],o[1]))
+        cv2.rectangle(result, (o[2][0],o[2][1]), (o[2][2],o[2][3]), (255,178,50), 8)
     
-    cv2.imwrite("test.jpg", result)
+    cv2.imwrite("test2.jpg", result)
    
 
 if __name__ == "__main__":
